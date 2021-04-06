a1cfb0a754e4905f2638d7b072b95fbb77c36313
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-06 16:39:46 -0700

    commit tree 6fa035aa93c3d869a962677a812f7c15dcfdd1cf

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arr, name) {
+    return arr.filter(item => item.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(arr, nameBeginsWith) {
+    return arr.filter(item => item.slice(0, nameBeginsWith.length) === nameBeginsWith)
+    // return arr.filter(item => nameBeginsWith[0] === item[0] && nameBeginsWith[1] === item[1])
+}
+
+function matchName(arr, name) {
+    return arr.filter(item => name === item.name)
+}
